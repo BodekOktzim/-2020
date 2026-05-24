ef5b78b21fcc67ca5d6c9793e44e0d5259e062f4
BodekOktzim <bodek1oktzim@gmail.com>
2026-05-24 12:35:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,65 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const DB_FILE = path.join(__dirname, 'facebook.db');
+
+const DRIVE_URL =
+'https://dl.dropboxusercontent.com/scl/fi/2lexnf75czsrap1zxm3za/facebook.db-2020?rlkey=72a5jth0f255sgix7376fa20w';
+
+app.use(express.static('public'));
+
+async function downloadDatabase() {
+
+    try {
+
+        if (fs.existsSync(DB_FILE)) {
+
+            fs.unlinkSync(DB_FILE);
+
+            console.log('Old DB deleted');
+        }
+
+        console.log('Downloading database...');
+
+        const response = await axios({
+            method: 'GET',
+            url: DRIVE_URL,
+            responseType: 'stream'
+        });
+
+        const writer = fs.createWriteStream(DB_FILE);
+
+        response.data.pipe(writer);
+
+        return new Promise((resolve, reject) => {
+
+            writer.on('finish', () => {
+
+                console.log('Database downloaded');
+
+                const stats = fs.statSync(DB_FILE);
+
+                console.log('DB SIZE:', stats.size);
+
+                resolve();
+            });
+
+            writer.on('error', reject);
+        });
+
+    } catch (err) {
+
+        console.log('DOWNLOAD ERROR:', err);
+    }
+}
+
 app.get('/search', async (req, res) => {
 
     try {
@@ -68,3 +130,15 @@ app.get('/search', async (req, res) => {
         });
     }
 });
+
+async function startServer() {
+
+    await downloadDatabase();
+
+    app.listen(PORT, () => {
+
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+startServer();
